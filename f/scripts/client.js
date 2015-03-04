f607dac15deb8b9faeb060661ccf8edd2b9564fc
okalintu <ilkka.oksanen@aalto.fi>
2015-03-04 02:45:33 +0200

    implemented feature to allow ppl to remove themselves from que

--- f/scripts/client.js
+++ f/scripts/client.js
@@ -2,13 +2,16 @@ var ioc = angular.module('ioc',[]);
 
 ioc.controller('ioctrl',function($scope){
 	var soc= io();
+	var pw =0;
+
+	$scope.uid =0;
 	$scope.que=[];
 	$scope.row="";
 	$scope.name="";
 	$scope.qsize=0;
 	
 	$scope.submit = function(){
-		soc.emit("queadd",{"row":$scope.row,"name":$scope.name});
+		soc.emit("queadd",{"row":$scope.row,"name":$scope.name,"uid":$scope.uid});
 		$scope.row="";
 		$scope.name="";
 	};
@@ -19,8 +22,20 @@ ioc.controller('ioctrl',function($scope){
 		$scope.$apply();
 	});
 
+	soc.on("userinfo",function(data){
+		$scope.uid = data.uid;
+		pw= data.pw;
+	});
+
+	soc.on("err",function(err){
+		console.log("error! msg: "+err.msg);
+	});
+
 	$scope.popfirst = function(){
 		soc.emit("quepopfirst","");	
 	};
-
+	$scope.querm = function(qid){
+		data = {"qid":qid,"pw":pw};
+		soc.emit("quedelete",data);	
+	};
 });
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var que = []
+var que = [];
+var pws = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/f/templates/index.html');
@@ -18,10 +19,19 @@ app.get('/f/*', function(req, res){
 
 
 io.on('connection', function(socket){
+  var uid = Date.now();
+  var pw =  Math.random()*10000000 + Math.random()*10000000 -1;
+  pws[uid] = pw;
+
+
+  socket.emit("userinfo",{"uid":uid,"pw":pw});
   socket.emit("quedata",que);
+  
    
   socket.on("queadd",function(data){
-		console.log("adding new que entry");
+		console.log("adding new que entry with user :" );
+    data.qid= Date.now();
+
   	que.push(data);
 		io.emit("quedata",que);
   });
@@ -29,9 +39,22 @@ io.on('connection', function(socket){
 		console.log("deleting first from que");
   	que.shift();
 		io.emit("quedata",que);
+
   });
-
-
+  socket.on("quedelete",function(data){
+    for (var i=0;i<que.length;i++){
+      if (que[i].qid == data.qid){
+        if (data.pw != pws[que[i].uid]){
+          socket.emit("err",{"msg":"you're not allowed to remove this entry"});
+          break;
+        }
+        console.log("deleting: "+ que[i].name);
+        que.splice(i,1);
+        io.emit("quedata",que);
+        break;
+      }
+    }
+  });
 
 });
 
